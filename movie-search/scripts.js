28a9e2c1de7e258c20eba6c8d01fc4e16a535ff4
dairylandjetfan <dairylandjetfan@gmail.com>
2018-05-24 18:13:35 -0500

    Latest Changes

--- movie-search/scripts.js
+++ movie-search/scripts.js
@@ -1,10 +1,13 @@
 function dynamicSort(property) {
     var sortOrder = 1;
+    
     if(property[0] === "-") {
+
         sortOrder = -1;
         property = property.substr(1);
     }
     return function (a,b) {
+
         var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
         return result * sortOrder;
     }
@@ -13,8 +16,8 @@ function dynamicSort(property) {
 $(function() {
 	// SEARCH/READ
 	$('#search-button').on('click', function(event) {
-		event.preventDefault();
 
+		event.preventDefault();
 		var serachInput = $('#search-input');
 
 		$.ajax({
@@ -22,19 +25,26 @@ $(function() {
 			method: 'GET',
 			contentType: 'application/json',
 			success: function(response) {
-				var tbodyEl = $('tbody');
 
+				var tbodyEl = $('tbody');
 				tbodyEl.html('');
 
-				if ( !response.results.Error) {
+				if ( !response.results.Error ) {
 
 					response.results.Search.sort(dynamicSort("Title"));
 
+					tbodyEl.append('<tr><td align="center">Place the mouse over the links to see their posters.</td>');
+
 					for (i = 0; response.results.Search.length > i; i++) {
+
 						tbodyEl.append('\
-							<li>' + response.results.Search[i].Title + ' - ' + response.results.Search[i].Year + '</li>\
-						');
+							<li><a href="#">' + response.results.Search[i].Title + ' - ' + response.results.Search[i].Year + '</a><div align="right" class="box"><iframe src="' + response.results.Search[i].Poster + '" width="300px" height="450px"></iframe></div></li>\
+						</tr>');
 					};
+
+				} else {
+
+					tbodyEl.append('<td align="center">NO RESULTS FOUND.</td>');
 				}
 			}
 		});
